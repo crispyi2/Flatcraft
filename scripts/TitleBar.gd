1a1a5f3206d08b250c014733ba4e768a0426bd50
crispyi2 <abdurtaalib@gmail.com>
2021-05-18 07:24:07 +0000

    custom title bar

--- scripts/TitleBar.gd
+++ scripts/TitleBar.gd
@@ -5,11 +5,16 @@ var dragging_start_position = Vector2()
 
 func _on_TitleBar_gui_input(event):
 	print(event)
-	
 	if event is InputEventMouseButton:
 		if event.get_button_index() == 1:
 			following = !following
 			dragging_start_position = get_local_mouse_position()
 			
 func _process(_delta):
-	
+	if following:
+		OS.set_window_position(OS.window_position + get_global_mouse_position() - dragging_start_position)
+
+
+
+func _on_Close_pressed():
+	get_tree().quit()
